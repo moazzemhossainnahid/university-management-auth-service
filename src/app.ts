754800868d0b5a94ed11a04a30a47f480f93d381
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-09-02 20:33:30 +0600

    create user successfully

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
+
 const app: Application = express()
 
 app.use(cors())
@@ -7,7 +9,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Application routes
+app.use('/api/v1/users', usersRouter)
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('Working Successfully!')
 })
 
